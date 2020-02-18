ac96b382097c6195127e4cfb0462a2d48f2142de
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-02-18 15:33:02 +0100

    :arrow_up: feat: Add ft_strlen in Assembly and other test file
    1) ft_strlen.asm
    2) ft_strcpy.asm in work
    3) copy_argv.asm (test file)

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
-char 	*start(char *);
+char 	*copy_argv(char *);
+int		ft_strlen(char *);
+char	*ft_strcpy(char *, char *);
 
 int		main(void)
 {
-	printf("%s\n", start("Assembleur !"));
+	printf("%s\n", copy_argv("Assembleur !"));
+	printf("%d\n", ft_strlen("Assembleur !"));
+//	printf("%s\n", ft_strcpy("ASM", "test"));
 	return (0);
 }
--- test.asm
+++
@@ -1,5 +0,0 @@
-global _start
-
-_start:
-	mov rax, rdi
-	ret
